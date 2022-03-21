ec8b95f94badd55c513c7d9e7bee332de7fd40db
Sporsho <94753278+SARDEEN-RAHMAN-SPORSHO@users.noreply.github.com>
2022-03-21 09:23:18 +0600

    Create ch-install.sh

---
+++ ch-install.sh
@@ -0,0 +1,38 @@
+pkg install python -y 
+echo " "
+pkg install python2 -y
+echo " "
+pkg install python3 -y
+echo " "
+pip install requests 
+echo " "
+pip install m
+echo " " 
+pip2 install requests 
+echo " "
+pip2 install m
+echo " " 
+pip3 install requests 
+echo " "
+pip3 install m
+echo " " 
+apt-get update -y
+echo " "
+apt-get upgrade -y
+echo " "
+pkg install figlet -y
+echo " "
+pkg install toilet -y
+echo " "
+pkg install cowsay -y
+echo " "
+pkg install nano -y
+echo " "
+pkg install ruby -y
+echo " "
+gem install lolcat
+echo " "
+pkg install nano -y
+echo ""
+pkg install mpv -y
+echo " "
