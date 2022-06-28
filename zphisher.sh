d9c49db37fa41d85ffa43c90c98988a5d4fb24ee
Sporsho <94753278+SARDEEN-RAHMAN-SPORSHO@users.noreply.github.com>
2022-06-28 15:18:27 +0600

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -57,26 +57,25 @@ kill_pid() {
 banner() {
 	cat <<- EOF
 		${GREEN} 
-		${GREEN}              _     _     _               
-		${GREEN}                                   | |   (_)   | |              
-		${RED}| |  | | | | '_ \ / _ \ '__|  ____          _ __ | |__  _ ___| |__   ___ _ __ 
-		${RED}| |__| |_| | |_) |  __/ |    |_____|         | '_ \| '_ \| / __| '_ \ / _ \ '__|
-		${RED} \____\__, |_.__/ \___|_|                             | |_) | | | | \__ \ | | |  __/ |   
-		${GREEN}       | .__/|_| |_|_|___/_| |_|\___|_|   
-		${GREEN}       | |                                
-		${GREEN}       |_|                ${RED}Version : 2.2
+		${GREEN} ____      _                               _     _     _               
+		${GREEN}/ ___|   _| |__   ___ _ __                | |   (_)   | |              
+		${RED}.| |  | | | | '_ \ / _ \ '__|  ____    ____ | |__  _ ___| |__   ___ _ __ 
+		${RED} | |__| |_| | |_) |  __/ |    |_____| | '_ \| '_ \| / __| '_ \ / _ \ '__|
+		${RED}  \____\__, |_.__/ \___|_|            | |_) | | | | \__ \ | | |  __/ |   
+		${GREEN}     |___/                          | .__/|_| |_|_|___/_| |_|\___|_|   
+		${GREEN}                                    | |                                
+		${GREEN}                                    |_|                ${RED}Version : 1.0.1
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Cyber Software Development & IT Security Inc. (Sardeen Rahman Sporsho) ${WHITE}
 	EOF
 }
 
 ## Small Banner
 banner_small() {
 	cat <<- EOF
-		${GREEN}
-		${RED}     ░▀▀█░█▀█░█░█░▀█▀░█▀▀░█░█░█▀▀░█▀▄
-		${BLUE}    ░▄▀░░█▀▀░█▀█░░█░░▀▀█░█▀█░█▀▀░█▀▄
-		${ORANGE}  ░▀▀▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀${WHITE} 2.2
+		${GREEN}    ⬛⬛⬛    ░█▀█░█░█░▀█▀░█▀▀░█░█░█▀▀░█▀▄
+		${BLUE}     ⬛   ➖➖ ░█▀▀░█▀█░░█░░▀▀█░█▀█░█▀▀░█▀▄
+		${ORANGE}   ⬛⬛⬛    ░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀${WHITE} 1.0.1
 	EOF
 }
 
